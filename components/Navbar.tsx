0e32733182c41a467b19a3adddafb24b83054589
Mau Reyes <reyes.marq@gmail.com>
2020-09-28 19:42:44 -0600

    [wip] navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
 
+let style: React.CSSProperties = {
+  width: '100%',
+  height: '200px',
+};
+
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <h1>Navbar</h1>
+    <nav
+      className="navbar is-primary-nav"
+      role="navigation"
+      aria-label="main navigation"
+      style={style}
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              width={112}
+              height={28}
+            />
+          </a>
+          <a
+            role="button"
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
+        </div>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <a className="navbar-item">Home</a>
+              <a className="navbar-item">About us</a>
+              <a className="navbar-item">Services</a>
+              <a className="navbar-item">Reviews</a>
+              <a className="navbar-item">About our team</a>
+              <div className="buttons" style={{border: "1px solid black"}}>
+                <a className="button is-info">Log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
