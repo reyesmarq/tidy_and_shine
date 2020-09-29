7e59b7beab2b6139e4894b6fccbd4790d815ae25
Mau Reyes <reyes.marq@gmail.com>
2020-09-28 20:18:17 -0600

    [feat] navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,61 +1,53 @@
 import Navbar from 'trunx/component/Navbar';
 import Button from 'trunx/component/Button';
 import Buttons from 'trunx/component/Buttons';
+import Container from 'trunx/component/Container';
 
-let style: React.CSSProperties = {
-  width: '100%',
-  height: '200px',
-};
+let navbarItem: React.CSSProperties = {
+  marginRight: '40px'
+}
 
-const Nav: React.FC = () => (
-  <Navbar>
-    <Navbar.Brand>
-      <Navbar.Item href="https://bulma.io">
-        <img
-          src="https://bulma.io/images/bulma-logo.png"
-          width="112"
-          height="28"
-        />
-      </Navbar.Item>
-    </Navbar.Brand>
-
-    <Navbar.Burger />
-
-    <Navbar.Menu>
-      <Navbar.Start>
-        <Navbar.Item>Home</Navbar.Item>
-
-        <Navbar.Item>Documentation</Navbar.Item>
-
-        <Navbar.Item hasDropdown isHoverable>
-          <Navbar.Link>More</Navbar.Link>
-
-          <Navbar.Dropdown>
-            <Navbar.Item>About</Navbar.Item>
-
-            <Navbar.Item>Jobs</Navbar.Item>
-
-            <Navbar.Item>Contact</Navbar.Item>
-
-            <Navbar.Divider />
-
-            <Navbar.Item>Report an issue</Navbar.Item>
-          </Navbar.Dropdown>
+const Nav = () => (
+  <Navbar
+    style={{
+      width: '100%',
+      height: '200px',
+      border: '1px solid red',
+    }}
+  >
+    <Container>
+      <Navbar.Brand>
+        <Navbar.Item href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+          />
         </Navbar.Item>
-      </Navbar.Start>
+      </Navbar.Brand>
 
-      <Navbar.End>
-        <Navbar.Item>
-          <Buttons>
-            <Button isPrimary>
-              <strong>Sign up</strong>
-            </Button>
+      <Navbar.Burger />
 
-            <Button isLight>Log in</Button>
-          </Buttons>
-        </Navbar.Item>
-      </Navbar.End>
-    </Navbar.Menu>
+      <Navbar.Menu>
+        <Navbar.End>
+          <Navbar.Item>
+            <div className="navbar-items-main" style={{display: 'flex', borderBottom: "1px solid black"}}>
+              <Navbar.Item>Home</Navbar.Item>
+              <Navbar.Item>About us</Navbar.Item>
+              <Navbar.Item>Services</Navbar.Item>
+              <Navbar.Item>Reviews</Navbar.Item>
+              <Navbar.Item>About our team</Navbar.Item>
+            </div>
+
+            <Buttons>
+              <Button isInfo isSmall style={{marginLeft: '50px'}}>
+                {'Booking'.toUpperCase()}
+              </Button>
+            </Buttons>
+          </Navbar.Item>
+        </Navbar.End>
+      </Navbar.Menu>
+    </Container>
   </Navbar>
 );
 
